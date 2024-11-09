3a7d032d9620c7708b674b40ee83dcc82d8beb44
mohamed-abdelrehem <mohameddabdelrehem@gmail.com>
2024-11-09 18:38:50 +0200

    Added the userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,10 +6,10 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from "./components/MainContent";
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       <WelcomeMessage />
@@ -35,9 +35,12 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <MainContent />
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+      
       <Footer />
     </>
   );
 }
-
 export default App
