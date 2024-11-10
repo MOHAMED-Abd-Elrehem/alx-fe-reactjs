8910503c0ba5e9c67cd1e58c891c41be2cf1ab09
mohamed-abdelrehem <mohameddabdelrehem@gmail.com>
2024-11-10 23:20:36 +0200

    Added inline style

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+      <footer style= {{backgroundColor:"#f5f5f5", color: "#000000", fontSize: "18px", padding:"15px"}} >
+        <p>© 2023 City Lovers</p>
+      </footer>
+    );
+}
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+  return (
+    <main style= {{backgroundColor:"#f5f5f5", color: "#000000", fontSize: "22px", padding: "15px"}}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+export default MainContent;
