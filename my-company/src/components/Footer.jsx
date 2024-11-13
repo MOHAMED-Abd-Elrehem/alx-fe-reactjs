ee11aef33016b8e382b42efe96e57154448eac53
mohamed-abdelrehem <mohameddabdelrehem@gmail.com>
2024-11-14 00:02:10 +0200

    Modified

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#f5f5f5",
+        color: "#000000",
+        fontSize: "18px",
+        padding: "15px",
+        marginTop: "10px",
+        textAlign: "center",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ my-company/src/components/Services.jsx
@@ -0,0 +1,15 @@
+function Services() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1 style={{ paddingBottom: "30px" }}>Our Services</h1>
+      <ul
+        style={{ listStyle: "none", fontWeight: "bold", paddingBottom: "20px" }}
+      >
+        <li style={{ paddingBottom: "8px" }}>Technology Consulting</li>
+        <li style={{ paddingBottom: "8px" }}>Market Analysis</li>
+        <li>Product Development</li>
+      </ul>
+    </div>
+  );
+}
+export default Services;
