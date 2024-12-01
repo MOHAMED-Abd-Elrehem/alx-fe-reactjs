aaeb68f01c717e80312658d1c57f5323681653e6
mohamed-abdelrehem <mohameddabdelrehem@gmail.com>
2024-12-01 18:12:34 +0200

    Updated

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 rounded-lg shadow-lg p-8 max-w-sm">
+    <div className="user-profile my-20 bg-gray-100 rounded-lg shadow-lg p-8 max-w-sm">
       <img className="rounded-full w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">
