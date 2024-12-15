ea9601b0121300627bd91d2a7eb1c813875623b7
mohamed-abdelrehem <mohameddabdelrehem@gmail.com>
2024-12-15 16:19:27 +0200

    Modified

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -21,7 +21,7 @@ const Search = () => {
       const data = await fetchUserData(username);
       setUserData(data);
     } catch {
-      setError("Looks like we can't find the user.");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
