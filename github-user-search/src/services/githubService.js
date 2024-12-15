2f2b9ed8167e07beb9cfdaec0cc73acd591ba482
mohamed-abdelrehem <mohameddabdelrehem@gmail.com>
2024-12-15 18:17:29 +0200

    Modified

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -5,7 +5,7 @@ const fetchUserData = async ({userName, location, minRepos, page = 1 }) => {
     if (userName) query += `${userName} in:login`
     if (location) query += `location:${location}`
     if (minRepos) query += `repos:${minRepos} `
-    const API_URL = `https://api.github.com/users?q=${query}&page=${page}&per_page=20 `;
+    const API_URL = `https://api.github.com/search/users?q=${query}&page=${page}&per_page=20 `;
     const response = await axios.get(API_URL);
 return response.data;
 };
